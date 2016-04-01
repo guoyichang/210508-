ee65051bc97e7722aa6ffec4e16b9d383b71ec1e
guoyichang <15153511628@163.com>
2016-04-01 09:15:15 +0800

    添加

---
+++ GITDemo/GITDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GITDemo
+//
+//  Created by jredu on 16/4/1.
+//  Copyright © 2016年 jereh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GITDemo/GITDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GITDemo
+//
+//  Created by jredu on 16/4/1.
+//  Copyright © 2016年 jereh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+     
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
